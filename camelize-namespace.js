168e063f5c3d9de5adf0e4adf13a41423ba48d2b
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2014-07-10 18:58:28 +0800

    Added camelize-namespace.js file.

---
+++ camelize-namespace.js
@@ -0,0 +1,23 @@
+/*:
+	@module-configuration:
+		{
+			"packageName": "camelize-namespace",
+			"fileName": "camelize-namespace.js",
+			"moduleName": "camelizeNamespace",
+			"authorName": "Richeve S. Bebedor",
+			"authorEMail": "richeve.bebedor@gmail.com",
+			"repository": "git@github.com:volkovasystems/camelize-namespace.git",
+			"testCase": "camelize-namespace-test.js",
+			"isGlobal": true
+		}
+	@end-module-configuration
+
+	@module-documentation:
+
+	@end-module-documentation
+*/
+var camelizeNamespace = function camelizeNamespace( namespace ){
+
+};
+
+( module || { } ).exports = camelizeNamespace;
