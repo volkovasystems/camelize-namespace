a5d6cf273597e485d1c92c99997cddca428c1a72
Richeve Bebedor <richeve.bebedor@gmail.com>
2014-07-11 13:25:22 +0800

    Finished the implementation of camelizeNamespace procedure.

--- camelize-namespace.js
+++ camelize-namespace.js
@@ -16,17 +16,35 @@
 
 	@end-module-documentation
 */
-var camelizeNamespace = function camelizeNamespace( namespace ){
+var camelizeNamespace = function camelizeNamespace( namespace, doNotCamelizeFirstLetter ){
 	/*:
 		@meta-configuration:
 			{
-				"namespace:required": "string"
+				"namespace:required": "string",
+				"doNotCamelizeFirstLetter:optional": boolean
 			}
 		@end-meta-configuration
 	*/
 
 	if( NAMESPACE_PATTERN.test( namespace ) ){
-
+        return namespace
+            .toLowerCase( )
+            .replace( NAMESPACE_TERM_PATTERN,
+                function onReplaced( match, divideCharacter ){
+                    if( divideCharacter ){
+                        return match.replace( divideCharacter, "" ).toUpperCase( );
+                    }else{
+                        return match.toUpperCase( );
+                    }
+                } )
+            .replace( FIRST_LETTER_PATTERN,
+                function onReplaced( match ){
+                    if( doNotCamelizeFirstLetter ){
+                        return match.toLowerCase( );
+                    }else{
+                        return match;
+                    }
+                } );
 	}else{
 		var error = new Error( "invalid namespace format" );
 		console.error( error );
@@ -35,6 +53,7 @@ var camelizeNamespace = function camelizeNamespace( namespace ){
 };
 
 const NAMESPACE_PATTERN = /^(?:[a-zA-Z][a-zA-Z0-9]+[-_ ])*[a-zA-Z][a-zA-Z0-9]+$/;
-const NAMESPACE_TERM_PATTERN = /^([a-zA-Z])|[-_ ]([a-zA-Z])/;
+const NAMESPACE_TERM_PATTERN = /^[a-zA-Z]|([-_ ])[a-zA-Z]/g;
+const FIRST_LETTER_PATTERN = /^[a-zA-Z]/;
 
 ( module || { } ).exports = camelizeNamespace;
